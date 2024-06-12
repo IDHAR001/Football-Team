a976182bd55ecb8e9a260a0f201d54dde56c44ff
IDHAR001 <943967062@qq.com>
2024-06-12 23:36:22 +0800

    update llm

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import streamlit as st
+from llm import chat
 
+st.set_page_config(page_title="IDHAR - Football Team",
+                   page_icon="🦾",
+                   layout="wide",
+                   initial_sidebar_state="collapsed")
+st.title('Football Team')
 ICON_IMAGE = "data/logo.png"
 st.logo(ICON_IMAGE)
-st.sidebar.markdown("Hi!")
+st.sidebar.markdown("Hi! I'm idhar!")
 
-st.title('Football Team')
+# prompt = st.sidebar.chat_input("Say sth")
 
+# if prompt:
+#     st.sidebar.write(f"User said: {prompt}")
+with st.sidebar:
+    messages = st.container()
+    if prompt := st.chat_input("Say something"):
+        anwser = chat(prompt)
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(anwser)
+ 
 player_info = {
     'age': st.number_input('Age'),
     'height': st.number_input('Height(cm)'),
--- llm.py
+++ llm.py
@@ -8,5 +8,15 @@ MOONSHOT_API_KEY = load_dotenv('./.env')
 llm = OpenAI(
     api_key = os.environ[f'MOONSHOT_API_KEY'],
     base_url = "https://api.moonshot.cn/v1",
-    temperature = 0.3
 )
+
+def chat(prompt):
+    completion = llm.chat.completions.create(
+        model = "moonshot-v1-8k",
+        messages = [
+            {"role": "system", "content": "你是 idhar，一个提供足球阵容解决方案的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。"},
+            {"role": "user", "content": f"{prompt}"}
+        ],
+        temperature = 0.3,
+    )
+    return completion.choices[0].message.content
